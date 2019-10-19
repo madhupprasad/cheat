f5987294d90f53bb1ff2be84dfe5d03e00c00f2c
Madhu <38370391+madhupprasad@users.noreply.github.com>
2019-10-19 12:14:05 +0530

    Update Dij.py

--- Dij.py
+++ Dij.py
@@ -1,8 +1,4 @@
-# Python program for Dijkstra's single 
-# source shortest path algorithm. The program is 
-# for adjacency matrix representation of the graph 
 
-# Library for INT_MAX 
 import sys 
 
 class Graph(): 
@@ -17,26 +13,16 @@ class Graph():
 		for node in range(self.V): 
 			print node, "\t", dist[node] 
 
-	# A utility function to find the vertex with 
-	# minimum distance value, from the set of vertices 
-	# not yet included in shortest path tree 
 	def minDistance(self, dist, sptSet): 
 
-		# Initilaize minimum distance for next node 
 		min = sys.maxint 
-
-		# Search not nearest vertex not in the 
-		# shortest path tree 
 		for v in range(self.V): 
 			if dist[v] < min and sptSet[v] == False: 
 				min = dist[v] 
 				min_index = v 
 
 		return min_index 
-
-	# Funtion that implements Dijkstra's single source 
-	# shortest path algorithm for a graph represented 
-	# using adjacency matrix representation 
+ 
 	def dijkstra(self, src): 
 
 		dist = [sys.maxint] * self.V 
@@ -45,22 +31,13 @@ class Graph():
 
 		for cout in range(self.V): 
 
-			# Pick the minimum distance vertex from 
-			# the set of vertices not yet processed. 
-			# u is always equal to src in first iteration 
 			u = self.minDistance(dist, sptSet) 
 
-			# Put the minimum distance vertex in the 
-			# shotest path tree 
+ 
 			sptSet[u] = True
 
-			# Update dist value of the adjacent vertices 
-			# of the picked vertex only if the current 
-			# distance is greater than new distance and 
-			# the vertex in not in the shotest path tree 
 			for v in range(self.V): 
-				if self.graph[u][v] > 0 and sptSet[v] == False and \ 
-				dist[v] > dist[u] + self.graph[u][v]: 
+				if self.graph[u][v] > 0 and sptSet[v] == False and 	dist[v] > dist[u] + self.graph[u][v]: 
 						dist[v] = dist[u] + self.graph[u][v] 
 
 		self.printSolution(dist) 
@@ -79,5 +56,3 @@ g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
 		]; 
 
 g.dijkstra(0); 
-
-# This code is contributed by Divyanshu Mehta 
